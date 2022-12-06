314c13424e49a1aded663b36cdde1bb2acee7cff
Trohat <petr.sed@centrum.cz>
2022-12-06 06:12:33 +0100

    Day 6

---
+++ D6/index.js
@@ -0,0 +1,34 @@
+console.log("AOC 2022 - Day 6: Tuning Trouble");
+
+const splitLines = data => data.split(String.fromCharCode(10));
+
+const task1 = datastream => {
+    for (let i = 4; i < datastream.length; i++) {
+        let marker = new Set(datastream.slice(i-4, i));
+        if (marker.size === 4) return i;
+    }
+};
+
+const task2 =  datastream => {
+    for (let i = 14; i < datastream.length; i++) {
+        let marker = new Set(datastream.slice(i-14, i));
+        if (marker.size === 14) return i;
+    }
+};
+
+let testdata = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`;
+
+console.log("");
+
+doEqualTest(task1(testdata), 7);
+
+console.time("Task 1");
+console.log("Task 1: " + task1(inputdata));
+console.timeEnd("Task 1");
+
+console.log("");
+
+doEqualTest(task2(testdata), 19);
+console.time("Task 2");
+console.log("Task 2: " + task2(inputdata));
+console.timeEnd("Task 2");
