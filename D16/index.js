625534f9006b638eb9d9501742fe8b6c41ab0618
Trohat <petr.sed@centrum.cz>
2022-12-16 17:38:02 +0100

    Bad solution

--- D16/index.js
+++ D16/index.js
@@ -92,9 +92,8 @@ const task1 = scan => {
         const stateString = states.shift();
         let at = stateString.split("--")[0];
 
-        const valves = Object.keys(scan);
-        const atIndex = valves.indexOf(at);
-        valves.splice(atIndex, 1);
+        let valves = Object.keys(scan);
+        valves = valves.removeAll(at);
 
         for (const valveString of valves) {
             let [at, open, mins, flow, total] = stateString.split("--");
@@ -120,20 +119,125 @@ const task1 = scan => {
                 open = divideAndSort(open);
                 let newStateString = `${valveString}--${open}--${mins}--${flow}`;
                 if (isBetterState(visited, newStateString, total)) {
-                        visited.set(newStateString, total);
-                        states.push(`${newStateString}--${total}`);
-                    }
+                    visited.set(newStateString, total);
+                    states.push(`${newStateString}--${total}`);
+                }
             }
         }
     }
     console.log(bestOpen);
     return best;
 }
-    
-const task2 = data => {
 
+const allowedTotals = [0, 20, 61, 102, 143, 184, 159, 192, 246, 300, 354, 408, 462, 516, 570, 624, 700, 776, 852, 928, 1007, 1086, 1165, 1246, 1327, 1408, 1489, 1570, 1651];
+const allowedOpens = ["", "DD", "DDJJ", "BBDDJJ", "BBDDHHJJ", , "DDHHJJ", "BBCDDHHJJ", "BBCCDDEEHHJJ"];
+
+const task2 = scan => {
+    let steps = 0;
+    let stateString = "AA--AA--0----0--0";
+    let visited = new Map();
+    visited.set(stateString, 0);
+    let states = [stateString + "--0"];
+    let best = 0;
+    while (states.length > 0) {
+        steps++;
+        const stateString = states.shift();
+        let [at, where, when, open, mins, flow, total] = stateString.split("--");
+        when = +when;
+        mins = +mins;
+        flow = +flow;
+        total = +total;
+
+        let valves = Object.keys(scan);
+        valves = valves.removeAll(at);
+        valves = valves.removeAll(where);
+        valves = valves.removeAll("AA");
+        for (let i = 0; i < open.length; i += 2) {
+            valves = valves.removeAll(open[0] + open[1]);
+        }
+        if (valves.length === 0) {
+            total += flow * (26 - mins);
+            if (total > best) {
+                best = total;
+            }
+            continue;
+        }
+
+        for (let valveString of valves) {
+            let [at, where, when, open, mins, flow, total] = stateString.split("--");
+            when = +when;
+            mins = +mins;
+            flow = +flow;
+            total = +total;
+
+            if (true && allowedTotals.includes(total)) {
+                if (total !== 0) console.log(stateString);
+
+
+                let to = scan[valveString].valves[at];
+                const newMins = Math.min(when, to);
+                let secondMins = Math.abs(when - to);
+                if (newMins === when) {
+                    let temp = valveString;
+                    valveString = where;
+                    where = temp;
+                }
+                let newValveObj = scan[valveString];
+                to = newValveObj.valves[at]; //back is same length
+
+                // what about second one?
+                if (mins + newMins + 1 >= 26) {
+                    total += flow * (26 - mins);
+                    if (total > best) {
+                        best = total;
+                    }
+                    continue;
+                }
+
+                if (newMins === 0) {
+
+                } else if (to === when) {
+                    total += flow * (newMins + 1);
+                    if (open.indexOf(valveString) === -1 || open.indexOf(valveString) % 2 === 1) {
+                        open += valveString;
+                        flow += newValveObj.rate;
+                    }
+                    if (open.indexOf(where) === -1 || open.indexOf(where) % 2 === 1) {
+                        open += where;
+                        flow += scan[where].rate;
+                    }
+
+                    open = divideAndSort(open);
+                    secondMins = 0;
+                    mins += newMins + 1;
+                }
+                else {
+                    total += flow * (newMins + 1);
+                    if (open.indexOf(valveString) === -1 || open.indexOf(valveString) % 2 === 1) {
+                        open += valveString;
+                        flow += newValveObj.rate;
+                    }
+                    open = divideAndSort(open);
+                    secondMins = secondMins - 1;
+                    mins += newMins + 1;
+                }
+                let newStateString = `${valveString}--${where}--${secondMins}--${open}--${mins}--${flow}`;
+                if (isBetterState(visited, newStateString, total)) {
+                    visited.set(newStateString, total);
+                    states.push(`${newStateString}--${total}`);
+                }
+            }
+        }
+    }
+    return best;
 }
-
+/*
+let testdata = `Valve AA has flow rate=0; tunnels lead to valves BB, CC, DD
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves AA, BB
+Valve DD has flow rate=20; tunnels lead to valves AA, EE
+Valve EE has flow rate=3; tunnels lead to valves DD`
+*/
 let testdata = `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 Valve BB has flow rate=13; tunnels lead to valves CC, AA
 Valve CC has flow rate=2; tunnels lead to valves DD, BB
@@ -149,7 +253,7 @@ const prepare = R.pipe(
     splitLines,
     parse,
     transform,
-    findAllShortest
+    
 );
 
 testdata = prepare(testdata);
@@ -164,15 +268,20 @@ console.log(inputdata);
 
 console.log("");
 
+console.time("Task 1 test");
 doEqualTest(task1(testdata), 1651);
+console.timeEnd("Task 1 test");
 
 console.time("Task 1");
-console.log("Task 1: " + task1(inputdata));
+// console.log("Task 1: " + task1(inputdata));
 console.timeEnd("Task 1");
 
 console.log("");
 
-//doEqualTest(task2(testdata), 336);
+console.time("Task 2 - test");
+// doEqualTest(task2(testdata), 1707);
+console.timeEnd("Task 2 - test");
+
 //console.time("Task 2");
-//console.log("Task 2: " + task2(inputdata));
+console.log("Task 2: " + task2(inputdata));
 //console.timeEnd("Task 2");
